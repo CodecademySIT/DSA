5c102688a1d543639cf04c2376ac25117a0f7e43
SayantaniDeb <74983536+SayantaniDeb@users.noreply.github.com>
2021-09-20 21:04:54 +0530

    Initial commit

---
+++ Python/First.py
@@ -0,0 +1,8 @@
+#Write a program to print whether a number is even or odd, also take input
+
+num =int(input("Enter your number: "))
+
+if(num%2==0):
+    print(num,"is even")
+else:
+    print(num,"is odd")
